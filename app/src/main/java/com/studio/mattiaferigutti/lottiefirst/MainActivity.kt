0e1fce09bb791a505a5182861498d0d4d7d37abb
Mattia Ferigutti <mattiaferigutti@gmail.com>
2020-11-21 15:53:49 +0100

    initial commit

---
+++ app/src/main/java/com/studio/mattiaferigutti/lottiefirst/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.studio.mattiaferigutti.lottiefirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        motionLayout.addTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, progress: Float) {
+                //catch every progress change
+                Log.d("progress", progress.toString())
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+            }
+
+        })
+
+    }
+}
